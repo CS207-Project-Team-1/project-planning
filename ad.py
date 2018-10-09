f3ccedf60c0254911a5f9d09579f333baf77cfc9
WFU <wfu@college.harvard.edu>
2018-10-08 22:02:47 -0400

    [proto] added differentiation for addition and multiplication

--- ad.py
+++ ad.py
@@ -18,31 +18,49 @@ class Expression(object):
 
     def d(self, feed_dict):
         '''Evaluates the derivative at the points given, returns to user'''
-        return self._d(feed_dict, dict())
+        return self._d(feed_dict, dict(), dict())
 
-    def _d(self):
-        '''Helper - Evaluates the differentiation products recursively.'''
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        '''Helper - Evaluates the differentiation products recursively.
+        @param: feed_dict: dictionary mapping var names 
+        @param: e_cache_dict: cache for previously evaluated values
+        @param: d_cache_dict: cache for previously calculated derivatives
+        '''
         raise NotImplementedError
 
     def __add__(self, other):
         try:
             # Propagate the need for gradient if one thing needs gradient
-            return Addition(self, other, grad=(self.grad and other.grad))
+            # Need to call other.grad first since self.grad may shortcircuit
+            return Addition(self, other, grad=(other.grad and self.grad))
         except AttributeError:
             return Addition(self, Constant(other), grad=self.grad)
     
     def __radd__(self, other):
         return self.__add__(other)
 
+    def __sub__(self, other):
+        try:
+            return Subtraction(self, other, grad=(other.grad and self.grad))
+        except AttributeError:
+            return Subtraction(self, Constant(other), grad=self.grad)
+
+    def __rsub__(self, other):
+        try:
+            return Subtraction(other, self, grad=(other.grad and self.grad))
+        except AttributeError:
+            return Subtraction(Constant(other), self, grad=self.grad)
+
     def __mul__(self, other):
         try:
-            return Multiplication(self, other, grad=(self.grad and other.grad))
+            return Multiplication(self, other, grad=(other.grad and self.grad))
         except AttributeError:
             return Multiplication(self, Constant(other), grad=self.grad)
     
     def __rmul__(self, other):
         # TODO: Multiplication not commutative if we enable matrix support
         return self.__mul__(other)
+    
 
 class Variable(Expression):
     def __init__(self, name, grad=True):
@@ -58,6 +76,9 @@ class Variable(Expression):
             return feed_dict[self.name]
         else:
             raise ValueError('Unbound variable %s' % self.name)
+    
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        return 1.0 
 
 
 class Constant(Expression):
@@ -68,6 +89,9 @@ class Constant(Expression):
     
     def _eval(self, feed_dict, cache_dict):
         return self.val
+
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        return 0
 
 
 class Binop(Expression):
@@ -87,6 +111,13 @@ class Addition(Binop):
             cache_dict[id(self)] = res1 + res2
         return cache_dict[id(self)]
 
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        if id(self) not in d_cache_dict:
+            d1 = self.expr1._d(feed_dict, e_cache_dict, d_cache_dict)
+            d2 = self.expr2._d(feed_dict, e_cache_dict, d_cache_dict)
+            d_cache_dict[id(self)] = d1 + d2
+        return d_cache_dict[id(self)]
+            
 
 class Subtraction(Binop):
     '''Subtraction, in the form A - B'''
@@ -96,6 +127,13 @@ class Subtraction(Binop):
             res2 = self.expr2._eval(feed_dict, cache_dict)
             cache_dict[id(self)] = res1 - res2
         return cache_dict[id(self)]
+    
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        if id(self) not in d_cache_dict:
+            d1 = self.expr1._d(feed_dict, e_cache_dict, d_cache_dict)
+            d2 = self.expr2._d(feed_dict, e_cache_dict, d_cache_dict)
+            d_cache_dict[id(self)] = d1 - d2
+        return d_cache_dict[id(self)]
 
 
 class Multiplication(Binop):
@@ -106,6 +144,15 @@ class Multiplication(Binop):
             res2 = self.expr2._eval(feed_dict, cache_dict)
             cache_dict[id(self)] = res1 * res2
         return cache_dict[id(self)]
+    
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        if id(self) not in d_cache_dict:
+            d1 = self.expr1._d(feed_dict, e_cache_dict, d_cache_dict)
+            d2 = self.expr2._d(feed_dict, e_cache_dict, d_cache_dict)
+            res1 = self.expr1._eval(feed_dict, e_cache_dict)
+            res2 = self.expr2._eval(feed_dict, e_cache_dict)
+            d_cache_dict[id(self)] = res1 * d2 + res2 * d1
+        return d_cache_dict[id(self)]
 
 
 class Division(Binop):
@@ -116,4 +163,12 @@ class Division(Binop):
             res2 = self.expr2._eval(feed_dict, cache_dict)
             cache_dict[id(self)] = res1 / res2
         return cache_dict[id(self)]
-
+    
+    def _d(self, feed_dict, e_cache_dict, d_cache_dict):
+        if id(self) not in d_cache_dict:
+            d1 = self.expr1._d(feed_dict, e_cache_dict, d_cache_dict)
+            d2 = self.expr2._d(feed_dict, e_cache_dict, d_cache_dict)
+            res1 = self.expr1._eval(feed_dict, e_cache_dict)
+            res2 = self.expr2._eval(feed_dict, e_cache_dict)
+            d_cache_dict[id(self)] = (d1 / res2) - (d2 * res1 / (res2 * res2))
+        return d_cache_dict[id(self)]
